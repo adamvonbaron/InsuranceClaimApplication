46e04bac90f3bf71188c636d0e6bfdfa7e326446
adam <akessle1@butler.edu>
2017-01-30 19:40:05 -0500

    add data abount sql server

---
+++ InsuranceApplication/InsuranceApplication/Classes/SQLDriver.cs
@@ -0,0 +1,11 @@
+﻿/* adam 2017-01-30 SQLDriver.cs */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsuranceApplication.Classes {
+    class SQLDriver {
+    }
+}
--- InsuranceApplication/InsuranceApplication/Classes/readme.txt
+++ InsuranceApplication/InsuranceApplication/Classes/readme.txt
@@ -1,1 +1,6 @@
 ﻿store classes related to project here
+
+SQLDriver.cs:
+username: charlesroot
+password: Ieamainsuranceclaim123
+SQL Server URL: insuranceclaim.database.windows.net
