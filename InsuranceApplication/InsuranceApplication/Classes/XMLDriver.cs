4b86651ac5b3fcc3d9edc2a542e823d88c64ab0b
adam <akessle1@butler.edu>
2017-02-01 16:43:21 -0500

    start working on xml driver

---
+++ InsuranceApplication/InsuranceApplication/Classes/XMLDriver.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace InsuranceApplication.Classes {
+    class XMLDriver {
+        public static String SerializeXML<T>(T input) {
+            string xml = null;
+            using (StringWriter sw = new StringWriter()) {
+                XmlSerializer xs = new XmlSerializer(typeof(T));
+                xs.Serialize(sw, input);
+                try {
+                    xml = sw.ToString();
+                } catch (Exception ex) {
+                    throw ex;
+                }
+            }
+            return xml;
+        }
+
+        public static T DeserializeXML<T>(string xml) {
+            XmlSerializer xs = new XmlSerializer(typeof(T));
+            using(StringReader sr = new StringReader(xml)) {
+                using(var xmlread = XmlReader.Create(sr)) {
+                    return (T)xs.Deserialize(xmlread);
+                }
+            }
+        }
+    }
+}
