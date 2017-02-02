1870ed4bfff6f4732f26471fd340e6dab8246dab
kyimer <kyimer@butler.edu>
2017-02-01 20:41:01 -0500

    start

---
+++ InsuranceApplication/InsuranceApplication/Classes/FinanceManger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsuranceApplication.Classes
+{
+    class FinanceManger
+    {
+           //constructors
+            public FinanceManger(string FirstName, string LastName, string Username, string PassWord, int UserRank)
+            {
+
+            }
+
+
+            //methods
+
+
+        
+        }
+    }
+
+
