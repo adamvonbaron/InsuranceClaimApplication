cb02dac49e455640a0e78fed326762fab32d3a39
adam <akessle1@butler.edu>
2017-02-01 21:42:11 -0500

    changes for stuff

--- InsuranceApplication/InsuranceApplication/Forms/MainWindow.Designer.cs
+++ InsuranceApplication/InsuranceApplication/Forms/MainWindow.Designer.cs
@@ -271,6 +271,7 @@
             this.btnSubmit.TabIndex = 15;
             this.btnSubmit.Text = "Submit";
             this.btnSubmit.UseVisualStyleBackColor = true;
+            this.btnSubmit.Click += new System.EventHandler(this.btnSubmit_Click);
             // 
             // formFormDemo
             // 
--- InsuranceApplication/InsuranceApplication/Forms/MainWindow.cs
+++ InsuranceApplication/InsuranceApplication/Forms/MainWindow.cs
@@ -9,64 +9,57 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using InsuranceApplication.Classes;
 
-namespace InsuranceApplication
-{
-    public partial class formFormDemo : Form
-    {
-        public formFormDemo()
-        {
+namespace InsuranceApplication {
+    public partial class formFormDemo : Form {
+        public formFormDemo() {
             InitializeComponent();
         }
 
-        private void btnExit_Click(object sender, EventArgs e)
-        {
+        public string username = null;
+
+        private void btnExit_Click(object sender, EventArgs e) {
             Application.Exit();
         }
 
-        private void formFormDemo_Load(object sender, EventArgs e)
-        {
+        private void formFormDemo_Load(object sender, EventArgs e) {
             // TODO: This line of code loads data into the 'insuranceClaimDataSet.users' table. You can move, or remove it, as needed.
             this.usersTableAdapter.Fill(this.insuranceClaimDataSet.users);
 
         }
 
-        private void txtFirstName_TextChanged(object sender, EventArgs e)
-        {
+        private void txtFirstName_TextChanged(object sender, EventArgs e) {
 
         }
 
-        private void txtLastName_TextChanged(object sender, EventArgs e)
-        {
+        private void txtLastName_TextChanged(object sender, EventArgs e) {
 
         }
 
-        private void txtUserName_TextChanged(object sender, EventArgs e)
-        {
+        private void txtUserName_TextChanged(object sender, EventArgs e) {
 
         }
 
-        private void txtPassword_TextChanged(object sender, EventArgs e)
-        {
+        private void txtPassword_TextChanged(object sender, EventArgs e) {
 
         }
 
-        private void txtRank_TextChanged(object sender, EventArgs e)
-        {
+        private void txtRank_TextChanged(object sender, EventArgs e) {
 
         }
 
-        private void txtCreation_TextChanged(object sender, EventArgs e)
-        {
+        private void txtCreation_TextChanged(object sender, EventArgs e) {
 
         }
 
-        private void txtClaims_TextChanged(object sender, EventArgs e)
-        {
+        private void txtClaims_TextChanged(object sender, EventArgs e) {
 
         }
 
-        private void dgvUsers_CellContentClick(object sender, DataGridViewCellEventArgs e)
-        {
+        private void dgvUsers_CellContentClick(object sender, DataGridViewCellEventArgs e) {
+
+        }
+
+        private void btnSubmit_Click(object sender, EventArgs e) {
 
         }
     }
