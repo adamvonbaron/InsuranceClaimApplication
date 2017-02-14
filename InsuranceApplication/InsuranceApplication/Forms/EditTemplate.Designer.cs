4e9356840ebcf126bff638082ec51dade9511c13
Domenic O'Neill <doneill1@butler.edu>
2017-02-13 21:14:07 -0500

    Gui for Edit profile

--- InsuranceApplication/InsuranceApplication/Forms/EditTemplate.Designer.cs
+++ InsuranceApplication/InsuranceApplication/Forms/EditTemplate.Designer.cs
@@ -28,19 +28,160 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblFName = new System.Windows.Forms.Label();
+            this.txtFName = new System.Windows.Forms.TextBox();
+            this.btnUpdateFName = new System.Windows.Forms.Label();
+            this.btnSaveFName = new System.Windows.Forms.Label();
+            this.btnSaveLName = new System.Windows.Forms.Label();
+            this.btnUpdateLName = new System.Windows.Forms.Label();
+            this.txtLName = new System.Windows.Forms.TextBox();
+            this.lblLName = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(58, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "First Name:";
+            // 
+            // lblFName
+            // 
+            this.lblFName.AutoSize = true;
+            this.lblFName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFName.Location = new System.Drawing.Point(179, 49);
+            this.lblFName.Name = "lblFName";
+            this.lblFName.Size = new System.Drawing.Size(39, 17);
+            this.lblFName.TabIndex = 1;
+            this.lblFName.Text = "John";
+            this.lblFName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtFName
+            // 
+            this.txtFName.Location = new System.Drawing.Point(319, 46);
+            this.txtFName.Name = "txtFName";
+            this.txtFName.Size = new System.Drawing.Size(100, 20);
+            this.txtFName.TabIndex = 2;
+            this.txtFName.Visible = false;
+            // 
+            // btnUpdateFName
+            // 
+            this.btnUpdateFName.AutoSize = true;
+            this.btnUpdateFName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdateFName.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.btnUpdateFName.Location = new System.Drawing.Point(273, 49);
+            this.btnUpdateFName.Name = "btnUpdateFName";
+            this.btnUpdateFName.Size = new System.Drawing.Size(40, 13);
+            this.btnUpdateFName.TabIndex = 3;
+            this.btnUpdateFName.Text = "update";
+            this.btnUpdateFName.Click += new System.EventHandler(this.btnUpdateFName_Click);
+            // 
+            // btnSaveFName
+            // 
+            this.btnSaveFName.AutoSize = true;
+            this.btnSaveFName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSaveFName.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.btnSaveFName.Location = new System.Drawing.Point(273, 49);
+            this.btnSaveFName.Name = "btnSaveFName";
+            this.btnSaveFName.Size = new System.Drawing.Size(30, 13);
+            this.btnSaveFName.TabIndex = 4;
+            this.btnSaveFName.Text = "save";
+            this.btnSaveFName.Visible = false;
+            this.btnSaveFName.Click += new System.EventHandler(this.btnSaveFName_Click);
+            // 
+            // btnSaveLName
+            // 
+            this.btnSaveLName.AutoSize = true;
+            this.btnSaveLName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSaveLName.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.btnSaveLName.Location = new System.Drawing.Point(273, 81);
+            this.btnSaveLName.Name = "btnSaveLName";
+            this.btnSaveLName.Size = new System.Drawing.Size(30, 13);
+            this.btnSaveLName.TabIndex = 9;
+            this.btnSaveLName.Text = "save";
+            this.btnSaveLName.Visible = false;
+            this.btnSaveLName.Click += new System.EventHandler(this.btnSaveLName_Click);
+            // 
+            // btnUpdateLName
+            // 
+            this.btnUpdateLName.AutoSize = true;
+            this.btnUpdateLName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdateLName.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.btnUpdateLName.Location = new System.Drawing.Point(273, 81);
+            this.btnUpdateLName.Name = "btnUpdateLName";
+            this.btnUpdateLName.Size = new System.Drawing.Size(40, 13);
+            this.btnUpdateLName.TabIndex = 8;
+            this.btnUpdateLName.Text = "update";
+            this.btnUpdateLName.Click += new System.EventHandler(this.btnUpdateLName_Click);
+            // 
+            // txtLName
+            // 
+            this.txtLName.Location = new System.Drawing.Point(319, 78);
+            this.txtLName.Name = "txtLName";
+            this.txtLName.Size = new System.Drawing.Size(100, 20);
+            this.txtLName.TabIndex = 7;
+            this.txtLName.Visible = false;
+            // 
+            // lblLName
+            // 
+            this.lblLName.AutoSize = true;
+            this.lblLName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLName.Location = new System.Drawing.Point(179, 81);
+            this.lblLName.Name = "lblLName";
+            this.lblLName.Size = new System.Drawing.Size(43, 17);
+            this.lblLName.TabIndex = 6;
+            this.lblLName.Text = "Smith";
+            this.lblLName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(58, 81);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(90, 17);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Last Name:";
             // 
             // EditTemplate
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(497, 232);
+            this.ClientSize = new System.Drawing.Size(497, 349);
+            this.Controls.Add(this.btnSaveLName);
+            this.Controls.Add(this.btnUpdateLName);
+            this.Controls.Add(this.txtLName);
+            this.Controls.Add(this.lblLName);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnSaveFName);
+            this.Controls.Add(this.btnUpdateFName);
+            this.Controls.Add(this.txtFName);
+            this.Controls.Add(this.lblFName);
+            this.Controls.Add(this.label1);
             this.Name = "EditTemplate";
             this.Text = "EditTemplate";
+            this.Load += new System.EventHandler(this.EditTemplate_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblFName;
+        private System.Windows.Forms.TextBox txtFName;
+        private System.Windows.Forms.Label btnUpdateFName;
+        private System.Windows.Forms.Label btnSaveFName;
+        private System.Windows.Forms.Label btnSaveLName;
+        private System.Windows.Forms.Label btnUpdateLName;
+        private System.Windows.Forms.TextBox txtLName;
+        private System.Windows.Forms.Label lblLName;
+        private System.Windows.Forms.Label label6;
     }
 }
