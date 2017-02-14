0a1f7c1fd0f5ae50728e1fcca9d2f5f425506ef2
doneill1 <doneill1@D306549>
2017-02-14 17:16:19 -0500

    new User Class

---
+++ InsuranceApplication/InsuranceApplication/ClassesRefine/Client.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsuranceApplication.Classes
+{
+    class Client
+    {
+        //properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public int Rank { get; set; }
+
+        //constructor
+        public Client(string firstname, string lastname, string username,
+            string password, int rank)
+        {
+            this.FirstName = firstname;
+            this.LastName = lastname;
+            this.UserName = username;
+            this.Password = password;
+            this.Rank = rank;
+        }
+
+        //methods
+        public void ApplyClaim()
+        {
+
+        }
+        public void ViewMessages()
+        {
+
+        }
+        public void ViewStatus()
+        {
+
+        }
+        public void Upload()
+        {
+
+        }
+        public void EditProfile()
+        {
+
+        }
+    }
+}
