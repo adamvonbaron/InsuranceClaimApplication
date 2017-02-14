bc2f7f2be58869a06e99853358feb73a910ad510
adam <apk@space.cowboy>
2017-02-13 20:52:41 -0500

    registration for users works bird up drink from me eat from me my body is your communion I DIDNT GET THE JOB AT FRUIT LOOPS INVESTIGATE 311

--- InsuranceApplication/InsuranceApplication/Forms/FormDriver.cs
+++ InsuranceApplication/InsuranceApplication/Forms/FormDriver.cs
@@ -23,6 +23,8 @@ namespace InsuranceApplication {
         }
 
         private void formFormDemo_Load(object sender, EventArgs e) {
+            // TODO: This line of code loads data into the 'insuranceClaimDataSet.users' table. You can move, or remove it, as needed.
+            this.usersTableAdapter.Fill(this.insuranceClaimDataSet.users);
 
         }
 
@@ -72,24 +74,19 @@ namespace InsuranceApplication {
         }
 
         private void btnRegisterUser_Click(object sender, EventArgs e) {
-            string firstname = txtFirstName.Text;
-            string lastname = txtLastName.Text;
-            string username = txtUserName.Text;
-            string password = txtPassword.Text;
-            DateTime birthday = dtpBirthday.Value;
-            string phonenumber = txtPhoneNumber.Text;
-            XDocument xmldoc = new XDocument();
-            XElement xml = new XElement("user",
-                           new XElement("firstname", firstname),
-                           new XElement("lastname", lastname),
-                           new XElement("username", username),
-                           new XElement("password", password),
-                           new XElement("birthday", birthday),
-                           new XElement("phonenumber", phonenumber));
-            xmldoc.Add(xml);
-            xmldoc.Save("H:\\user_demo.xml");
-            int rowsaffected = database.InsertUser(xmldoc);
-            MessageBox.Show("Rows affected: " + rowsaffected, "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = database.conn;
+            cmd.CommandText = @"insert into users (firstname, lastname, username, password, birthday, phonenumber)
+                                values(@firstname, @lastname, @username, @password, @birthday, @phonenumber);";
+            cmd.Parameters.AddWithValue("@firstname", txtFirstName.Text);
+            cmd.Parameters.AddWithValue("@lastname", txtLastName.Text);
+            cmd.Parameters.AddWithValue("@username", txtUserName.Text);
+            cmd.Parameters.AddWithValue("@password", txtPassword.Text);
+            cmd.Parameters.AddWithValue("@birthday", dtpBirthday.Text);
+            cmd.Parameters.AddWithValue("@phonenumber", txtPhoneNumber.Text);
+            database.conn.Open();
+            cmd.ExecuteNonQuery();
+            database.conn.Close();
         }
     }
 }
