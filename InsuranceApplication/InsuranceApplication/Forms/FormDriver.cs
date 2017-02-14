655585f9d24d4f80e652aa02c897d7fb57667dcf
Domenic O'Neill <doneill1@butler.edu>
2017-02-13 22:08:48 -0500

    GUI changes

--- InsuranceApplication/InsuranceApplication/Forms/FormDriver.cs
+++ InsuranceApplication/InsuranceApplication/Forms/FormDriver.cs
@@ -25,7 +25,6 @@ namespace InsuranceApplication {
         private void formFormDemo_Load(object sender, EventArgs e) {
             // TODO: This line of code loads data into the 'insuranceClaimDataSet.users' table. You can move, or remove it, as needed.
             this.usersTableAdapter.Fill(this.insuranceClaimDataSet.users);
-
         }
 
         private void btnUpdateProfile_Click(object sender, EventArgs e) {
