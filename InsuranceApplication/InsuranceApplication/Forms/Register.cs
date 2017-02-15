c360a8a74041de714237b1ecb4c80d6daceefa18
alach <alach@D306550>
2017-02-15 12:42:39 -0500

    work on Register.cs for enter button

--- InsuranceApplication/InsuranceApplication/Forms/Register.cs
+++ InsuranceApplication/InsuranceApplication/Forms/Register.cs
@@ -25,18 +25,8 @@ namespace InsuranceApplication.Forms
 
         private void btnEnter_Click(object sender, EventArgs e)
         {
-            //string[] lines = {txtFirstname.Text+" "+txtLastname.Text};
-            //System.IO.File.WriteAllLines(@"H:\SE361\Register.txt", lines);
+            //Classes.libsql.RegisterUser(txtFirstname.Text, txtLastname.Text, txtUsername.Text, txtPassword.Text, txtDOB.Text, txtPhone.Text);
 
-            ////Open File
-            //TextWriter tw = new StreamWriter(@"H:\SE361\Register.txt");
-
-            ////Write to file
-            //tw.WriteLine("First Name: "+txtFirstname.Text+" Last Name: "+txtLastname.Text+" Username: "+txtUsername.Text+" Password: "+txtPassword.Text+
-            //    " Email: "+txtEmail.Text+" Date of Birth: "+txtDOB.Text+" Phone: "+txtPhone.Text+" Address: "+txtAddress.Text);
-
-            ////Close File
-            //tw.Close();
             using (StreamWriter w = File.AppendText(@"H:\SE361\Register.txt"))
             {
                 w.WriteLine("First Name: " + txtFirstname.Text + " Last Name: " + txtLastname.Text + " Username: " + txtUsername.Text + " Password: " + txtPassword.Text +
