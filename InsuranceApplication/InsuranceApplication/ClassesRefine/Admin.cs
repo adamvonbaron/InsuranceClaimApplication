201188c7086fa03bb4f968c53e67041fe9404c7b
typhoun <typhoun@butler.edu>
2017-02-27 20:03:39 -0500

    saving periodically

--- InsuranceApplication/InsuranceApplication/ClassesRefine/Admin.cs
+++ InsuranceApplication/InsuranceApplication/ClassesRefine/Admin.cs
@@ -8,10 +8,28 @@ namespace InsuranceApplication.ClassesRefine
 {
     class Admin
     {
+        //properties
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
         public int Rank { get; set; }
+
+        //constructor
+        public Admin(string firstname, string lastname, string username, string password, int rank)
+        {
+            this.FirstName = firstname;
+            this.LastName = lastname;
+            this.UserName = username;
+            this.Password = password;
+            this.Rank = rank;
+        }
+
+        //methods
+        public void Login(string username, string password)
+        {
+
+        }
+
     }
 }
