a0f973c54607db156b0f47184dd5c732cbc654ae
typhoun <typhoun@butler.edu>
2017-03-01 20:04:16 -0500

    added claim manager class

---
+++ InsuranceApplication/InsuranceApplication/Classes/ClaimManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InsuranceApplication.Classes;
+
+namespace InsuranceApplication.Classes
+{
+    public class ClaimManager : User
+    {
+        public ClaimManager(string username, string password)
+                            : base(username, password)
+        {
+            UserData userdata = libsql.GetUserData(UserName);
+            FirstName = userdata.FirstName;
+            LastName = userdata.LastName;
+            Birthday = userdata.Birthday;
+            Phonenumber = userdata.Phonenumber;
+            Type = userdata.Type;
+        }
+
+        public ClaimManager(UserData userdata) : base(userdata) { }
+    }
+}
