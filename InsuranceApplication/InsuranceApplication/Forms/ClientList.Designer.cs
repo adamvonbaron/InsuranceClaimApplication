a4c39629f169e31788a86483d841d856e8d2b33a
typhoun <jphoun@butler.edu>
2017-03-15 21:23:50 -0400

    add

--- InsuranceApplication/InsuranceApplication/Forms/ClientList.Designer.cs
+++ InsuranceApplication/InsuranceApplication/Forms/ClientList.Designer.cs
@@ -28,11 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lstClientList = new System.Windows.Forms.ListBox();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lstClientList
+            // 
+            this.lstClientList.FormattingEnabled = true;
+            this.lstClientList.Location = new System.Drawing.Point(58, 31);
+            this.lstClientList.Name = "lstClientList";
+            this.lstClientList.Size = new System.Drawing.Size(401, 225);
+            this.lstClientList.TabIndex = 0;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(384, 299);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // ClientList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(522, 352);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.lstClientList);
+            this.Name = "ClientList";
             this.Text = "ClientList";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListBox lstClientList;
+        private System.Windows.Forms.Button btnExit;
     }
 }
